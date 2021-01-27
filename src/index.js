3e9d0844732197f7c9281ab51aed5633112d9526
Sunil Kumar <ksun34133@gmail.com>
2021-01-27 17:34:35 +0530

    Deploy add to gh pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
